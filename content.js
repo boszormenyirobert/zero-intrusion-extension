9f18db4952c13d91b619a383cad9dd3f77998c46
r62g8y <boszormenyirobert@yahoo.com>
2025-09-27 19:42:15 +0200

    fixes

--- content.js
+++ content.js
@@ -1,14 +1,6 @@
 
 console.log("Content script running!");
 
-
-chrome.runtime.sendMessage({
-    action: "fillLoginFields",
-    password: "MySecret123",
-    previous:  "asdf"
-});
- 
-
 chrome.runtime.onMessage.addListener((message) => {
     if (message.action === "fillLoginFields") {        
         console.log(message); 

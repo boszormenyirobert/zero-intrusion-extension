2acaa082630ef78121ad01b9b1db5f4fc685706a
r62g8y <boszormenyirobert@yahoo.com>
2025-10-26 13:46:07 +0100

    fixes

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // export const BASE_API_URL = 'https://easypublic.tegernseer-web.de';
-export const BASE_API_URL = 'http://192.168.178.89:8082';
+export const BASE_API_URL = 'http://82.165.219.9:8082';
